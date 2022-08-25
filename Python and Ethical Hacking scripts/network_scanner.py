91dd3cf2d9038886e22f89112ea17afa2e9b1522
wambugu-dennis <wambugudennis398@gmail.com>
2022-08-26 00:16:31 +0300

    initial network scanner commit

---
+++ Python and Ethical Hacking scripts/network_scanner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# created by Wambugu Dennis
+# 26-08-2022
+
+import scapy.all as copy
+import scapy.layers.l2
+
+
+def scan(ip):
+    scapy.layers.l2.arping(ip)
+
+
+scan("192.168.100.1/24")
