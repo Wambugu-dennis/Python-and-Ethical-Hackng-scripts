6da48b5a4dcb255a46e61c918494f7fbdf3fef3c
wambugu-dennis <wambugudennis398@gmail.com>
2022-08-26 01:05:45 +0300

    commit

--- Python and Ethical Hacking scripts/network_scanner.py
+++ Python and Ethical Hacking scripts/network_scanner.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
-# created by Wambugu Dennis
+# created by Wambugu
 # 26-08-2022
 
-import scapy.all as copy
-import scapy.layers.l2
+import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.layers.l2.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+
+    print(arp_request_broadcast.summary())
+    arp_request_broadcast.show()
 
 
-scan("192.168.1.1/24")
+scan("192.168.100.1/24")
