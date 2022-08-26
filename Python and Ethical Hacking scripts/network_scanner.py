8f6453a46f717136d87ccef9fcfebe2f6ea20792
wambugu-dennis <wambugudennis398@gmail.com>
2022-08-26 18:59:28 +0300

    commit 2

--- Python and Ethical Hacking scripts/network_scanner.py
+++ Python and Ethical Hacking scripts/network_scanner.py
@@ -4,6 +4,17 @@
 # 26-08-2022
 
 import scapy.all as scapy
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-I", "--iprange", dest="ip", help="ip address range to scan")
+    options = parser.parse_args()
+    if not options.ip:
+        parser.error("[-] Please specify an ip address range, use --help for more info.")
+    elif options.ip:
+        return options
 
 
 def scan(ip):
@@ -13,12 +24,21 @@ def scan(ip):
     # capture answered result packets in a list, set a time to end search and less verbose result
     answered_lst = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print("IP\t\t\t\tMAC ADDRESS\n-------------------------------------------------------------------")
-
+    clients_list = []  # list to contain all the dictionaries
     # loop through function to read all elements from answered list and display result
     for element in answered_lst:
-        print(element[1].psrc + "\t\t\t" + element[1].hwsrc)  # print result IP and MAC addresses in a table
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)  # append dictionaries containing data for each client to main list
+    return clients_list
+
+
+def print_result(results_list):
+    print("IP\t\t\t\tMAC ADDRESS\n-------------------------------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t\t" + client["mac"])  # print result IP and MAC addresses in a table
         print("-------------------------------------------------------------------")
 
 
-scan("192.168.1.1/24")
+options = get_arguments()
+scan_result = scan(options.ip)
+print_result(scan_result)
