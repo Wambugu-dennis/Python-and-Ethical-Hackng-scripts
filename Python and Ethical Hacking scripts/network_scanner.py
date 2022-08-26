da906a90089523663d5210b8f0dfc88369d3cf60
wambugu-dennis <wambugudennis398@gmail.com>
2022-08-26 16:27:54 +0300

    commit

--- Python and Ethical Hacking scripts/network_scanner.py
+++ Python and Ethical Hacking scripts/network_scanner.py
@@ -7,12 +7,18 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
+    arp_request = scapy.ARP(pdst=ip)  # get IPs of clients from arp broadcast request
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")  # send arp request to the broadcast address
+    arp_request_broadcast = broadcast / arp_request
+    # capture answered result packets in a list, set a time to end search and less verbose result
+    answered_lst = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
+    print("IP\t\t\t\tMAC ADDRESS\n-------------------------------------------------------------------")
+
+    # loop through function to read all elements from answered list and display result
+    for element in answered_lst:
+        print(element[1].psrc + "\t\t\t" + element[1].hwsrc)  # print result IP and MAC addresses in a table
+        print("-------------------------------------------------------------------")
 
 
 scan("192.168.1.1/24")
