3f675f4ca97caa486535363017cc8954ffb91a59
wambugu-dennis <wambugudennis398@gmail.com>
2022-08-30 02:05:46 +0300

    commit 1

---
+++ Python and Ethical Hacking scripts/arp_spoofer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# created by Wambugu
+# 30-08-2022
+
+import scapy.all as scapy
+
+packet = scapy.ARP(op=2, pdst="192.168.100.152", hwdst="92:9a:4a:07:d4:17", psrc="192.168.100.1")
