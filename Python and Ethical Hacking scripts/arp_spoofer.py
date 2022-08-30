a5f79a722a87d66f864b3c13e6e4d9c316ee7542
wambugu-dennis <wambugudennis398@gmail.com>
2022-08-30 21:29:48 +0300

    commit

--- Python and Ethical Hacking scripts/arp_spoofer.py
+++ Python and Ethical Hacking scripts/arp_spoofer.py
@@ -8,3 +8,5 @@ import scapy.all as scapy
 packet = scapy.ARP(op=2, pdst="192.168.100.152", hwdst="92:9a:4a:07:d4:17", psrc="192.168.100.1")
 print(packet.show())
 print(packet.summary())
+
+#  this is a test
