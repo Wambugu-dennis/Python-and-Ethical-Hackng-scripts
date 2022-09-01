aa465f3fcebb8a910619674c94866615d298b006
wambugu-dennis <wambugudennis398@gmail.com>
2022-09-02 00:01:37 +0300

    commit

--- Python and Ethical Hacking scripts/packet_sniffer.py
+++ Python and Ethical Hacking scripts/packet_sniffer.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import scapy.all as scapy
+from scapy.layers import http
 
 
 # created by Wambugu
 # 01-09-2022
 
 def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=sniffed_packet)
+    scapy.sniff(iface=interface, store=False, prn=sniffed_packet, filter="port 80")
 
 
 def sniffed_packet(packet):
