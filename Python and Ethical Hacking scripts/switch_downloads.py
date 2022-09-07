af3a3b56177f12a7ff210c5fe87663ba8568e28a
wambugu-dennis <wambugudennis398@gmail.com>
2022-09-08 01:20:39 +0300

    commit

--- Python and Ethical Hacking scripts/switch_downloads.py
+++ Python and Ethical Hacking scripts/switch_downloads.py
@@ -2,13 +2,23 @@
 import netfilterqueue
 import scapy.all as scapy
 
+
 def process_packet(packet):
     my_packet = scapy.IP(packet.get_payload())
-    if my_packet.haslayer(scapy.DNSRR):
+    if my_packet.haslayer(scapy.Raw):
+        if my_packet[scapy.TCP].dport == 80:
+            print("[+] HTTP Request...")
+            if ".exe" in my_packet[scapy.Raw].load:
+                print("[+] .exe Request file requested")
 
-        packt.accept()
+            print(my_packet.show())
+        elif my_packet[scapy.TCP].sport == 80:
+            print("[+] HTTP Response")
+            print(my_packet.show())
+
+    packt.accept()
 
 
 queue = netfilterqueue.NetfiletrQueue()
-queue.bind(0), process_packet)
+queue.bind(0, process_packet)
 queue.run()
