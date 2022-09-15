5b4787bd1426a6294d3b1266431531a8d3a935a4
wambugu-dennis <wambugudennis398@gmail.com>
2022-09-15 14:47:24 +0300

    commit

---
+++ Python and Ethical Hacking scripts/code_injector.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import netfilterqueue
+import scapy.all as scapy
+
+ack_list = []
+
+
+def set_load(packet, load):
+    my_packet[scapy.Raw].load = load
+    del my_packet[scapy.IP].len
+    del my_packet[scapy.IP].chksum
+    del my_packet[scapy.TCP].chksum
+    return packet
+
+
+def process_packet(packet):
+    my_packet = scapy.IP(packet.get_payload())
+    if my_packet.haslayer(scapy.Raw):
+        if my_packet[scapy.TCP].dport == 80:
+            print("[+] Request...")
+            print(my_packet.show())
+        elif my_packet[scapy.TCP].sport == 80:
+            print("[+] Response...")
+            print(my_packet.show())
+
+    packet.accept()
+
+
+queue = netfilterqueue.NetfiletrQueue()
+queue.bind(0, process_packet)
+queue.run()
