a1d2d5fc8c2670a6f2d3298d58d5cee1bec63222
wambugu-dennis <wambugudennis398@gmail.com>
2022-09-18 23:59:09 +0300

    commit

--- Python and Ethical Hacking scripts/sslstrip.py
+++ Python and Ethical Hacking scripts/sslstrip.py
@@ -1,4 +1,1 @@
-#  this strips off https in unsecure site
-#  this has however become obsolete due to security update on various platforms and browsers
-#  tools like better cap are used together with this program for better testing output --hstshijack
 #  use this program and all other programs in this repository for learning purposes only!
