04257d93828d54241757ad8bdc75939b2afba2e1
wambugu-dennis <wambugudennis398@gmail.com>
2022-09-30 14:01:22 +0300

    update web crawler

---
+++ Python and Ethical Hacking scripts/Website Hacking/webcrawler.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import requests
+
+
+def request(url):
+    try:
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+
+target_url = "google.com"
+subdomains_list = ""
+with open("/home/bitsec/Downloads/subdomains-wordlist.txt", "r") as wordlist_file:
+    print("[+] ----- Valid subdomain(s) -----   ")
+    for line in wordlist_file:
+        word = line.strip()
+        test_url = word + "." + target_url
+        response = request(test_url)
+        if response:
+            subdomains_lists = subdomains_list + test_url
+            print(subdomains_lists)
+
