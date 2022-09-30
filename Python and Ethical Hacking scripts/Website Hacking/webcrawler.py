579c7ba5210bf28ff4f0bb36beba97bc1d16dd09
wambugu-dennis <wambugudennis398@gmail.com>
2022-09-30 14:23:21 +0300

    commit lists

--- Python and Ethical Hacking scripts/Website Hacking/webcrawler.py
+++ Python and Ethical Hacking scripts/Website Hacking/webcrawler.py
@@ -12,13 +12,21 @@ def request(url):
 
 target_url = "google.com"
 subdomains_list = ""
-with open("/home/bitsec/Downloads/subdomains-wordlist.txt", "r") as wordlist_file:
+# directories_list = ""
+with open("/home/bitsec/Downloads/files-and-dirs-wordlist.txt", "r") as wordlist_file:
     print("[+] ----- Valid subdomain(s) -----   ")
+
     for line in wordlist_file:
         word = line.strip()
         test_url = word + "." + target_url
         response = request(test_url)
+        # for discovering directories
+        # test_dir = target_url + "/" + word
+        # response = request(test_dir)
+
         if response:
+            # for subdomains
             subdomains_lists = subdomains_list + test_url
             print(subdomains_lists)
-
+            # for directories
+            # directories_list = directories_list + test_dir
