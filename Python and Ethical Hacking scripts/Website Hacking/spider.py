9bae11b6730f49b29b8795bfccc4affaa2e1a8c0
wambugu-dennis <wambugudennis398@gmail.com>
2022-10-02 02:45:17 +0300

    keyboard interrupt

--- Python and Ethical Hacking scripts/Website Hacking/spider.py
+++ Python and Ethical Hacking scripts/Website Hacking/spider.py
@@ -2,29 +2,30 @@
 
 import requests
 import re
-import urllib.parse
+import urllib.parse as urlparse
 
 
-target_url = "https://zsecurity.org/"  # supply your own domain here
+target_url = "https://cybershujaa.co.ke/"  # supply your own domain here
 target_links = []
 def extract_links(url):
     response = requests.get(target_url)
-    return re.findall('(?:href=")(.*?)"', response.content.decode())
+    return re.findall('(?:href=")(.*?)"', response.content.decode(errors="ignore"))
 
 print("\n[+] ----- Discovered valid links and directories -----\n")
 def crawl(url):
     href_links = extract_links(url)
+    try:
+        for link in href_links:
+            link = urlparse.urljoin(url, link)
 
+            if "#" in link:
+                link = link.split("#")[0]
 
-    for link in href_links:
-        link = urllib.parse.urljoin(url, link)
-
-        if "#" in link:
-            link = link.split("#")[0]
-
-        if target_url in link and link not in target_links:
-            target_links.append(link)
-            print( str(link))
-            crawl(link)
+            if target_url in link and link not in target_links:
+                target_links.append(link)
+                print( str(link))
+                crawl(link)
+    except KeyboardInterrupt:
+        exit()
 
 crawl(target_url)
