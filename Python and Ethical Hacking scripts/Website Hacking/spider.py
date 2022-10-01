abe3ba6bce7d1d623f36fb9c93e7bf731dd406ad
wambugu-dennis <wambugudennis398@gmail.com>
2022-10-02 02:34:06 +0300

    commit spider

--- Python and Ethical Hacking scripts/Website Hacking/spider.py
+++ Python and Ethical Hacking scripts/Website Hacking/spider.py
@@ -2,14 +2,29 @@
 
 import requests
 import re
+import urllib.parse
 
-target_url = "http://zsecurity.org"
+
+target_url = "https://zsecurity.org/"
+target_links = []
 def extract_links(url):
     response = requests.get(target_url)
     return re.findall('(?:href=")(.*?)"', response.content.decode())
 
-href_links = extract_links(target_url)
-print("\n[+] ----- Discovered links -----\n")
-for link in href_links:
-    print( str(link))
+print("\n[+] ----- Discovered valid links and directories -----\n")
+def crawl(url):
+    href_links = extract_links(url)
 
+
+    for link in href_links:
+        link = urllib.parse.urljoin(url, link)
+
+        if "#" in link:
+            link = link.split("#")[0]
+
+        if target_url in link and link not in target_links:
+            target_links.append(link)
+            print( str(link))
+            crawl(link)
+
+crawl(target_url)
