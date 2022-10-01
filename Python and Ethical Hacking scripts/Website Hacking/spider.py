5b176bd39e0a498a07743d3674a8c68adaa2f356
wambugu-dennis <wambugudennis398@gmail.com>
2022-10-02 01:16:37 +0300

    commit amended crawler

--- Python and Ethical Hacking scripts/Website Hacking/spider.py
+++ Python and Ethical Hacking scripts/Website Hacking/spider.py
@@ -3,17 +3,13 @@
 import requests
 import re
 
+target_url = "http://zsecurity.org"
+def extract_links(url):
+    response = requests.get(target_url)
+    return re.findall('(?:href=")(.*?)"', response.content.decode())
 
-def request(url):
-    try:
-        return requests.get("http://" + url)
-    except requests.exceptions.ConnectionError:
-        pass
-
-
-target_url = "zsecurity.org"
-response = request(target_url)
-href_links = re.findall('(?:href=")(.*?)"', response.content.decode())
-
-print("\n[+] ----- Discovered links -----\n\n" + str(href_links))
+href_links = extract_links(target_url)
+print("\n[+] ----- Discovered links -----\n")
+for link in href_links:
+    print( str(link))
 
