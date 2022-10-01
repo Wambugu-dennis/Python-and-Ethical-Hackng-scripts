74cabb3b5c64cdbbb7ad4c9cc874bdf516b5aaf2
wambugu-dennis <wambugudennis398@gmail.com>
2022-10-01 18:42:45 +0300

    commit

--- Python and Ethical Hacking scripts/Website Hacking/webcrawler.py
+++ Python and Ethical Hacking scripts/Website Hacking/webcrawler.py
@@ -12,13 +12,23 @@ def request(url):
 
 target_url = "google.com"
 subdomains_list = ""
-with open("/home/bitsec/Downloads/subdomains-wordlist.txt", "r") as wordlist_file:
-    print("[+] ----- Valid subdomain(s) -----   ")
-    for line in wordlist_file:
-        word = line.strip()
-        test_url = word + "." + target_url
-        response = request(test_url)
-        if response:
-            subdomains_lists = subdomains_list + test_url
-            print(subdomains_lists)
+directories_list = ""
+final_url = ""
+with open("/home/bitsec/Downloads/subdomains-wordlist.txt", "r") as sub_file:
+    with open("/home/bitsec/Downloads/subdomains-wordlist.txt", "r") as dir_file:
+
+        print("[+] ----- Discovered Valid subdomain(s) and directories -----   ")
+        for sub_line in sub_file:
+            sub_d = line.strip()
+            test_url = sub_d + "." + target_url
+            sub_response = request(test_url)
+
+            for dir_line in dir_file:
+                dir_d = line.strip()
+                test_dir = target_url + "/" + dir_d
+                dir_response = request(test_url)
+
+            if sub_response and dir_response:
+                final_url = subdomains_list + directories_list
+                print(final_url)
 
