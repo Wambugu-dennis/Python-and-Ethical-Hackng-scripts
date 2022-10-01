57b55890e91d209f6d1cd7969197944bbc6267c3
wambugu-dennis <wambugudennis398@gmail.com>
2022-10-02 00:49:40 +0300

    commit

--- Python and Ethical Hacking scripts/Website Hacking/spider.py
+++ Python and Ethical Hacking scripts/Website Hacking/spider.py
@@ -12,9 +12,7 @@ def request(url):
 
 
 target_url = "zsecurity.org"
-
 response = request(target_url)
-
 href_links = re.findall('(?:href=")(.*?)"', response.content.decode())
 
 print("\n[+] ----- Discovered links -----\n\n" + str(href_links))
